9d882a24a37f4a4cc2bf37a87d9f18f95c5cb82f
GiritharanRamesh <102209175+GiritharanRamesh@users.noreply.github.com>
2022-03-25 15:13:08 +0530

    Update settings.py

--- hrms/core/settings.py
+++ hrms/core/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tekhrmsd01db',
         'USER': 'hrmsadmin',
-        'PASSWORD': 'Tek@1234$',
+        'PASSWORD': '******',
         'HOST': 'db.tekistic.com',
         'PORT': '3306',
     }
